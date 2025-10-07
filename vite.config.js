3f62514cb1350dbeb3ddba4b023dd0d9e70b9aab
charan511 <musigumpulasaicharangoud123@gmail.com>
2025-10-07 19:42:44 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     // Optional: If you run into CORS with NewsAPI on free plan, consider adding a tiny proxy.
+   base: '/news-feed-dasshboard/',
     // proxy: { '/api': { target: 'https://newsapi.org', changeOrigin: true, rewrite: p => p.replace(/^\/api/, '') } }
   }
 })
